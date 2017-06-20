6bc2631ed67f8d4839d0a31384c879eaa75ee45b
Lukas Barth <mail@tinloaf.de>
2017-06-21 00:08:41 +0200

    Started documentation

--- src/rbtree.hpp
+++ src/rbtree.hpp
@@ -38,10 +38,20 @@ namespace iitree {
 }
 
 
-
+/**
+ * @brief Base class (template) to supply your node class with metainformation
+ *
+ * The class you use as nodes for the Red-Black Tree *must* derive from this
+ * class (template). It supplies your class with the necessary members to
+ * contain the linking between the tree nodes.
+ *
+ * @tparam Node       The node class itself. Yes, that's the class derived from this template. This sounds weird, but is correct. See the examples if you're confused.
+ * @tparam multiple   Boolean specifying whether the Red-Black Tree should support multiple elements that compare equally to each other. See RBTree documentation for details.
+ */
 template<class Node, bool multiple = false>
 class RBTreeNodeBase;
 
+/// @cond INTERNAL
 template<class Node>
 class RBTreeNodeBase<Node, true> {
 public:
@@ -66,7 +76,17 @@ public:
   Node *                  _rbt_right = nullptr;
   RBTreeNodeBase::Color   _rbt_color;
 };
+/// @endcond
 
+/**
+ * @brief   Helper base class for the NodeTraits you need to implement
+ *
+ * This class serves as an (optional) base class for the NodeTraits you need to
+ * implement. See DOCTODO for details on NodeTraits. This class just implements the
+ * various hooks as empty functions.
+ *
+ * @tparam Node   The node class of your Red Black Tree
+ */
 template<class Node>
 class RBDefaultNodeTraits {
 public:
@@ -78,6 +98,16 @@ public:
   static void swapped(Node & n1, Node & n2) {};
 };
 
+/**
+ * @brief The Red-Black Tree
+ *
+ * This is the main Red-Black Tree class.
+ *
+ * @tparam Node         The node class for this tree. It must be derived from RBTreeNodeBase. DOCTODO link
+ * @tparam NodeTraits   A class implementing various hooks and functions on your node class. See DOCTODO for details.
+ * @tparam multiple     A boolean specifying whether multiple elements that compare equally to each other (i.e. with the same key) may be inserted into the tree. If you set this to false and nonetheless insert multiple equal elements, undefined behaviour occurrs. However, if you know that this will not happen, setting this to false will speed up operations and save a little memory.
+ * @tparam Compare      A compare class. The Red-Black Tree follows STL semantics for 'Compare'. Defaults to std::less<Node>. Implement operator<(const Node & lhs, const Node & rhs) if you want to use it.
+ */
 template<class Node, class NodeTraits, bool multiple = false, class Compare = std::less<Node>>
 class RBTree
 {
@@ -87,17 +117,55 @@ public:
 
   RBTree();
 
-  // modifying the tree
+  /**
+   * @brief Inserts <node> into the tree
+   *
+   * Inserts <node> into the tree.
+   *
+   * *Warning*: Please note that after calling insert() on a node (and before
+   * removing that node again), that node *may not move in memory*. A common
+   * pitfall is to store nodes in a std::vector (or other STL container), which
+   * reallocates (and thereby moves objecs around).
+   *
+   * @param   Node  The node to be inserted.
+   */
   void insert(Node & node);
+
+  /**
+   * @brief Removes <node> from the tree
+   *
+   * Removes <node> from the tree.
+   *
+   * @param   Node  The node to be removed.
+   */
   void remove(Node & node);
 
   // Mainly debugging methods
+  /// @cond INTERNAL
   bool verify_integrity() const;
+  /// @endcond
+
+  /**
+   * @brief Debugging Method: Draw the Tree as a .dot file
+   *
+   * Outputs the current tree as a .dot file which can be drawn using
+   * graphviz.
+   *
+   * @param filename  The file path where to write the .dot file.
+   */
   void dump_to_dot(std::string & filename) const;
 
   // Iteration
+  /**
+   * @brief Iterator over elements in the tree
+   *
+   * This class represents an iterator over elements in a Red-Black tree. The iterator
+   * is an input iterator in terms of STL iterators, thus it provides only basic
+   * functionality.
+   */
   class const_iterator {
   public:
+    /// @cond INTERNAL
     typedef ptrdiff_t                         difference_type;
     typedef Node                              value_type;
     typedef const Node &                      const_reference;
@@ -126,10 +194,24 @@ public:
   private:
     Node * n;
     bool reverse;
+    /// @endcond
   };
+
+  /**
+   * Returns an iterator pointing to the smallest element in the tree.
+   */
   const_iterator cbegin() const;
+  /**
+   * Returns an iterator pointing after the largest element in the tree.
+   */
   const_iterator cend() const;
+  /**
+   * Returns an iterator pointing to the smallest element in the tree.
+   */
   const_iterator begin() const;
+  /**
+   * Returns an iterator pointing after the largest element in the tree.
+   */
   const_iterator end() const;
 
   // querying for contained elements
