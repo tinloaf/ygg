6ce0cd0131b313ed1eb4b283a8f0b13e9196f4d3
Lukas Barth <lukas.barth@kit.edu>
2018-03-09 11:10:47 +0100

    DynamicSegmentTree documentation

---
+++ docs/pages/DynamicSegmentTreeDoc.md
@@ -0,0 +1,32 @@
+@page dynamicsegmenttreedoc Introduction to the Dynamic Segment Tree
+
+The dynamic segment tree is something between a classic segment tree and Boost ICL's 
+[interval_map](http://www.boost.org/doc/libs/1_66_0/libs/icl/doc/html/index.html#boost_icl.introduction.definition_and_basic_example).
+
+It allows you to insert (and remove) intervals that are associated with a *value*. For every point 
+**x**
+in the key space, you can query for the *aggregate* of the values that contain **x**. You are 
+free to define what an aggregate is. If your values are numbers, a reasonable aggregate may be 
+the sum. If your values are lists of objects, an aggregate may be the concatenation of the lists, …
+
+This is more or less what Boost ICL's *interval_map* offers. On top of that, you can define (or 
+use the pre-defined) *combiners*. Combiners combine different aggregate values over an 
+interval in the key space. For example, if your values are numbers, the "max" function is a 
+reasonable (and in fact, pre-defined) combiner. Adding this combiner to your dynamic segment tree
+ allows you to efficiently (see TODO) query for the maximum aggregated value over any arbitrary 
+ interval in your key space. Possible combiners need to satisfy a couple of constraints, see TODO.
+ 
+An in-depth technical description of how the dynamic segment tree works can be found at TODO.
+
+Requirements
+------------
+
+* If you use `double` as key type, interval borders will be compared using `operator==` and 
+`operator!=`. Thus, if you want two intervals to begin / end at the same point, you must make 
+sure that their borders really contain the same `double`.
+* The aggregate type must offer a neutral element, which also must be the default-constructed value 
+of the type.
+* On the aggregate and value type, `operator+` and `operator-` must be defined. Together with the
+ requirement that you need a neutral element, this especially means that you need to have a 
+ "negative aggregate space": If `i` is the neutral element, and `x` is any element of your value 
+ / aggregate type, then `i - x` must also be valid in your aggregate type!
---
+++ examples/example_dynamic_segment_tree.cpp
@@ -0,0 +1,133 @@
+//
+// Created by lukas on 26.09.17.
+//
+
+#include "../src/ygg.hpp"
+
+using namespace ygg;
+
+/* First, we define which combiners we want to use. For this simple example, we use a
+ * simple provided MaxCombiner. See TODO for other combiners.
+ */
+using MCombiner = MaxCombiner<int>;
+using Combiners = CombinerPack<int, MCombiner>;
+
+/* The class representing our intervals and associated values.
+ *
+ * Our key space (i.e., the borders of the intervals) will be double.
+ *
+ * The while the values will be ints. The DynamicSegmentTree aggregates values by using
+ * operator+, thus our aggregation will be simply the sum of the aggregated values. We need to
+ * specify the type of the aggregate values, too.
+ *
+ *
+ */
+class Interval : public DynSegTreeNodeBase<double, int, int, Combiners>  {
+public:
+	double lower;   // lower interval border
+	double upper;   // upper interval border
+
+	int value;      // value associated with this interval.
+
+	Interval(double lower_in, double upper_in, int value_in) : lower(lower_in), upper(upper_in),
+	                                                           value(value_in) {};
+};
+
+/* We need to define traits on our interval class, derived from DynSegTreeNodeTraits<Interval>.
+ * This class tells the DynamicSegmentTree how to get the interval borders and the value
+ * from our Interval class.
+ */
+class IntervalTraits : public DynSegTreeNodeTraits<Interval> {
+public:
+	using key_type = double;
+	using value_type = int;
+
+	static key_type get_lower(const Interval & n) {
+		return n.lower;
+	}
+
+	static key_type get_upper(const Interval & n) {
+		return n.upper;
+	}
+
+	static value_type get_value(const Interval & n) {
+		return n.value;
+	}
+
+	/* All our intervals are right-open. The DynamicSegmentTree can handle every combination
+	 * of openness / closedness, and in fact can contain intervals of different types.
+	 */
+	static bool is_lower_closed(const Interval & n) {
+		(void)n;
+		return true;    // closed on the lower border
+	};
+	static bool is_upper_closed(const Interval & n) {
+		(void)n;
+		return false;   // open on the upper border
+	};
+};
+
+/* This is our DynamicSegmentTree
+ */
+using MyTree = DynamicSegmentTree<Interval, IntervalTraits, Combiners>;
+
+
+int main(int argc, char **argv) {
+	(void)argc;
+	(void)argv;
+
+	MyTree t;
+
+	// Storage for the actual intervals.
+	// WARNING: using STL containers here can backfire badly. See TODO.
+	std::vector<Interval> intervals;
+
+	/* We add the following interval / value combinations:
+	 *
+	 *  [0,   10)   -> 1
+	 *  [0.5, 10)   -> 2
+	 *  [10,  15)   -> 3
+	 *  [12,  20)   -> 8
+	 */
+	intervals.emplace_back(0.0, 10.0, 1);
+	intervals.emplace_back(0.5, 10.0, 2);
+	intervals.emplace_back(10.0, 15.0, 3);
+	intervals.emplace_back(12.0, 20.0, 8);
+	for (auto & interval : intervals) {
+		t.insert(interval);
+	}
+
+	/* Let's see what the aggregate value at the points 0, 0.5, 5, 10 and 15 is: */
+	for (auto point : std::vector<double>{0, 0.5, 5, 10, 14, 15}) {
+		std::cout << "Point: " << point << "\t| Aggregate Value: " << t.query(point) << "\n";
+	}
+
+	/* Should output:
+	 * Point: 0	  | Aggregate Value: 1
+	 * Point: 0.5	| Aggregate Value: 3
+	 * Point: 5	  | Aggregate Value: 3
+	 * Point: 10	| Aggregate Value: 3
+	 * Point: 15	| Aggregate Value: 8
+	 * */
+
+	/* Now, combiners. The max combiner first allows us to query (in O(1)) what the total maximum
+	 * value is: */
+	std::cout << "Maximum Value: " << t.get_combined<MCombiner>() << "\n";
+	/* Should output:
+	 * Maximum Value: 11
+	 */
+
+	/* But, we can also query (in O(log n)) the maximum over sub-ranges:*/
+	std::cout << "Maximum over [0, 10): " << t.get_combined<MCombiner>(0, 10) << "\n";
+	std::cout << "Maximum over [10, 12): " << t.get_combined<MCombiner>(10, 12) << "\n";
+	// We also can specify to query an interval closed on both sides:
+	std::cout << "Maximum over [10, 12]: " << t.get_combined<MCombiner>(10, 12, true, true) << "\n";
+
+	/* Overall output should be:
+	 * Maximum over [0, 10): 3
+	 * Maximum over [10, 12): 3
+	 * Maximum over [10, 12]: 11
+	 */
+
+	return 0;
+}
