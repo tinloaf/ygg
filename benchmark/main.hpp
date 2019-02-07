d69073ad77a90957e2f8f7ed20a2a190ce5f6972
Lukas Barth <lukas.barth@kit.edu>
2019-02-07 17:16:49 +0100

    Enable benchmark control

--- benchmark/main.hpp
+++ benchmark/main.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <utility>
 
 #include "common.hpp"
 
@@ -17,6 +18,10 @@ int main(int argc, char ** argv)
 {
 	int remaining_argc = argc;
 	char ** remaining_argv = (char **)malloc((size_t)argc * sizeof(char *));
+
+	int64_t experiment_size = EXPERIMENT_SIZE;
+	int64_t base_size = BASE_SIZE;
+	int64_t doublings = DOUBLINGS;
 	
 	remaining_argv[0] = argv[0];
 	size_t j = 1;
@@ -32,6 +37,18 @@ int main(int argc, char ** argv)
 			
 			i += 1;
 			remaining_argc -= 2;
+		} else if (strncmp(argv[i], "--doublings", strlen("--doublings")) == 0) {
+			doublings = (size_t)atoi(argv[i+1]);
+			i += 1;
+			remaining_argc -= 2;
+		} else if (strncmp(argv[i], "--base_size", strlen("--base_size")) == 0) {
+			base_size = (size_t)atoi(argv[i+1]);
+			i += 1;
+			remaining_argc -= 2;
+		} else if (strncmp(argv[i], "--experiment_size", strlen("--experiment_size")) == 0) {
+			experiment_size = (size_t)atoi(argv[i+1]);
+			i += 1;
+			remaining_argc -= 2;
 		} else {
 			remaining_argv[j++] = argv[i];
 		}
@@ -42,12 +59,16 @@ int main(int argc, char ** argv)
 	 * Register all tests
 	 */
 	auto plugins = DRAUP_GET_REGISTERED();
+	using RangeVector = std::vector<std::pair<int64_t, int64_t>>;
+	
 	DRAUP_FOR_EACH(plugins, {
 			auto * bench = new plugin();
+			RangeVector ranges;
+			ranges.emplace_back(base_size, base_size * (1 << doublings));
+			ranges.emplace_back(experiment_size, experiment_size);
 			bench->RangeMultiplier(2);
-			bench->Ranges({{BASE_SIZE, BASE_SIZE * (1 << DOUBLINGS)}, {EXPERIMENT_SIZE, EXPERIMENT_SIZE}});
-			::benchmark::internal::RegisterBenchmarkInternal(bench);
-	});
+			bench->Ranges(ranges);
+			::benchmark::internal::RegisterBenchmarkInternal(bench);});
 	
 	PAPI_STATS_WRITTEN = false;
 	
