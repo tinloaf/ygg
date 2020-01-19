d041e96f01696f2243adefdc9c6f9641951ae2b6
Lukas Barth <lukas.barth@kit.edu>
2020-01-19 16:33:52 +0100

    Unify tested trees

--- benchmark/bench_dst_move.cpp
+++ benchmark/bench_dst_move.cpp
@@ -46,48 +46,6 @@ BENCHMARK_DEFINE_F(MoveRBDSTFixture, BM_DST_Move)(benchmark::State & state)
 REGISTER(MoveRBDSTFixture, BM_DST_Move)
 
 /*
- * Weight-Balanced DST
- */
-using MoveWBDSTFixture = DSTFixture<WBDSTInterface<BasicDSTTreeOptions>,
-                                    MoveExperiment, false, true, true, false>;
-BENCHMARK_DEFINE_F(MoveWBDSTFixture, BM_DST_Move)(benchmark::State & state)
-{
-	PointerCountCallback::reset();
-	for (auto _ : state) {
-		size_t j = 0;
-		PointerCountCallback::start();
-		this->papi.start();
-		for (auto i : this->experiment_indices) {
-			this->t.remove(this->fixed_nodes[i]);
-			auto [upper, lower, value] = this->experiment_values[j++];
-			this->fixed_nodes[i].upper = upper;
-			this->fixed_nodes[i].lower = lower;
-			this->fixed_nodes[i].value = value; // TODO don't do this?
-			this->t.insert(this->fixed_nodes[i]);
-		}
-		this->papi.stop();
-		PointerCountCallback::stop();
-
-		state.PauseTiming();
-		// TODO actually, this is the same as above - also time it?
-		for (auto i : this->experiment_indices) {
-			this->t.remove(this->fixed_nodes[i]);
-			auto [upper, lower, value] = this->fixed_values[i];
-			this->fixed_nodes[i].upper = upper;
-			this->fixed_nodes[i].lower = lower;
-			this->fixed_nodes[i].value = value; // TODO don't do this?
-			this->t.insert(this->fixed_nodes[i]);
-		}
-		// TODO shuffling here?
-		state.ResumeTiming();
-	}
-
-	this->papi.report_and_reset(state);
-	PointerCountCallback::report(state);
-}
-REGISTER(MoveWBDSTFixture, BM_DST_Move)
-
-/*
  * Zip DST with hashing
  */
 using MoveZHDSTFixture = DSTFixture<
@@ -184,7 +142,8 @@ using Move32WBDSTFixture = DSTFixture<
     WBDSTInterface<BasicDSTTreeOptions, ygg::TreeFlags::WBT_DELTA_NUMERATOR<3>,
                    ygg::TreeFlags::WBT_DELTA_DENOMINATOR<1>,
                    ygg::TreeFlags::WBT_GAMMA_NUMERATOR<2>,
-                   ygg::TreeFlags::WBT_GAMMA_DENOMINATOR<1>>,
+                   ygg::TreeFlags::WBT_GAMMA_DENOMINATOR<1>,
+                   ygg::TreeFlags::WBT_SINGLE_PASS>,
     MoveExperiment, false, true, true, false>;
 BENCHMARK_DEFINE_F(Move32WBDSTFixture, BM_DST_Move)(benchmark::State & state)
 {
@@ -230,7 +189,8 @@ using MoveBalWBDSTFixture = DSTFixture<
     WBDSTInterface<BasicDSTTreeOptions, ygg::TreeFlags::WBT_DELTA_NUMERATOR<2>,
                    ygg::TreeFlags::WBT_DELTA_DENOMINATOR<1>,
                    ygg::TreeFlags::WBT_GAMMA_NUMERATOR<3>,
-                   ygg::TreeFlags::WBT_GAMMA_DENOMINATOR<2>>,
+                   ygg::TreeFlags::WBT_GAMMA_DENOMINATOR<2>,
+                   ygg::TreeFlags::WBT_SINGLE_PASS>,
     MoveExperiment, false, true, true, false>;
 BENCHMARK_DEFINE_F(MoveBalWBDSTFixture, BM_DST_Move)(benchmark::State & state)
 {
